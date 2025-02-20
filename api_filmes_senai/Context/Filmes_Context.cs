6660342cb8ef23885cb4ab00a4fe218daaa1b225
Yasmin Machado <yasmin.m.silva14@aluno.senai.br>
2025-02-20 08:15:44 -0300

    DOCS: Subindo atividade API

---
+++ api_filmes_senai/Context/Filmes_Context.cs
@@ -0,0 +1,27 @@
+﻿using api_filmes_senai.Domains;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace api_filmes_senai.Context
+{
+    public class Filmes_Context : DbContext
+    {
+        public Filmes_Context()
+        {
+            
+        }
+        public Filmes_Context(DbContextOptions<Filmes_Context>options): base(options) 
+        { 
+        }   
+        public DbSet<Genero> Genero { get; set; }
+        public DbSet<Filmes> Filmes { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder OptionsBuilder)
+        {
+            if (!OptionsBuilder.IsConfigured) {
+                OptionsBuilder.UseSqlServer("Server=Yasmin; Database=Filmes; Integrated Security=True; TrustServerCertificate=true; ");
+              }
+        }
+
+    }
+}
---
+++ api_filmes_senai/Domains/Genero.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace api_filmes_senai.Domains
+{
+    public class Genero
+    {
+
+        [Key]
+
+        public Guid IdGenero { get; set; }
+
+        [Column(TypeName = "VARACHAR (30)")]
+        [Required(ErrorMessage = "Nome do Gênero é obrigarório" )]
+        public string? Nome{ get; set; }
+    }
+}
